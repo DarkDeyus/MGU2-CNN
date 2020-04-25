e5dd6ccf9eb9f696ef35ac162fe659f562d5b90f
DarkDev <21362600+DarkDeyus@users.noreply.github.com>
2020-04-25 21:45:28 +0200

    Add CIFAR-100

--- Main.py
+++ Main.py
@@ -3,30 +3,76 @@ import tensorflow as tf
 import numpy as np
 import matplotlib.pyplot as plt
 from keras import Sequential
-from keras.datasets import cifar10
+from keras.datasets import cifar10, cifar100
 from keras.layers import Conv2D, MaxPooling2D, Dropout, Flatten, Dense, LeakyReLU, Activation
 from keras.preprocessing.image import ImageDataGenerator
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import confusion_matrix
+from enum import Enum
 import itertools
 from keras import backend as K
 
-NUM_CLASSES = 10
-cifar10_classes = ["airplane", "automobile", "bird", "cat", "deer", "dog", "frog", "horse", "ship", "truck"]
+
+class TestSet(Enum):
+    CIFAR10 = 1
+    CIFAR100 = 2
+
+
+CIFAR10_CLASSES = ["airplane", "automobile", "bird", "cat", "deer", "dog", "frog", "horse", "ship", "truck"]
+CIFAR100_CLASSES = [
+    'apple', 'aquarium_fish', 'baby', 'bear', 'beaver', 'bed', 'bee', 'beetle',
+    'bicycle', 'bottle', 'bowl', 'boy', 'bridge', 'bus', 'butterfly', 'camel',
+    'can', 'castle', 'caterpillar', 'cattle', 'chair', 'chimpanzee', 'clock',
+    'cloud', 'cockroach', 'couch', 'crab', 'crocodile', 'cup', 'dinosaur',
+    'dolphin', 'elephant', 'flatfish', 'forest', 'fox', 'girl', 'hamster',
+    'house', 'kangaroo', 'keyboard', 'lamp', 'lawn_mower', 'leopard', 'lion',
+    'lizard', 'lobster', 'man', 'maple_tree', 'motorcycle', 'mountain', 'mouse',
+    'mushroom', 'oak_tree', 'orange', 'orchid', 'otter', 'palm_tree', 'pear',
+    'pickup_truck', 'pine_tree', 'plain', 'plate', 'poppy', 'porcupine',
+    'possum', 'rabbit', 'raccoon', 'ray', 'road', 'rocket', 'rose',
+    'sea', 'seal', 'shark', 'shrew', 'skunk', 'skyscraper', 'snail', 'snake',
+    'spider', 'squirrel', 'streetcar', 'sunflower', 'sweet_pepper', 'table',
+    'tank', 'telephone', 'television', 'tiger', 'tractor', 'train', 'trout',
+    'tulip', 'turtle', 'wardrobe', 'whale', 'willow_tree', 'wolf', 'woman',
+    'worm'
+]
 
 INIT_LR = 1e-3  # initial learning rate
 BATCH_SIZE = 32
-EPOCHS = 20
+EPOCHS = 2
 VALIDATION_SPLIT = 0.2
 USE_GENERATOR = True
+SET = TestSet.CIFAR100
+
+
+def get_num_classes(test_set):
+    if test_set is TestSet.CIFAR10:
+        return 10
+    if test_set is TestSet.CIFAR100:
+        return 100
+
+
+def get_dataset(test_set):
+    if test_set is TestSet.CIFAR10:
+        return cifar10.load_data()
+    if test_set is TestSet.CIFAR100:
+        return cifar100.load_data()
+
+
+def get_labels(test_set):
+    if test_set is TestSet.CIFAR10:
+        return CIFAR10_CLASSES
+    if test_set is TestSet.CIFAR100:
+        return CIFAR100_CLASSES
 
 
 def normalise(x):
     return (x / 255) - 0.5
 
 
-def load_dataset():
-    (x_train_loaded, y_train_loaded), (x_test_loaded, y_test_loaded) = cifar10.load_data()
+def load_dataset(test_set):
+    (x_train_loaded, y_train_loaded), (x_test_loaded, y_test_loaded) = get_dataset(test_set)
+    num_classes = get_num_classes(test_set)
     # normalisation
     x_train_norm = normalise(x_train_loaded)
     x_test = normalise(x_test_loaded)
@@ -36,19 +82,21 @@ def load_dataset():
                                                                                                   test_size=VALIDATION_SPLIT,
                                                                                                   stratify=y_train_loaded)
     # One hot encode
-    y_train = keras.utils.to_categorical(y_train_to_categorical, NUM_CLASSES)
-    y_test = keras.utils.to_categorical(y_test_loaded, NUM_CLASSES)
-    y_validation = keras.utils.to_categorical(y_validation_to_categorical, NUM_CLASSES)
+    y_train = keras.utils.to_categorical(y_train_to_categorical, num_classes)
+    y_test = keras.utils.to_categorical(y_test_loaded, num_classes)
+    y_validation = keras.utils.to_categorical(y_validation_to_categorical, num_classes)
     return x_train, y_train, x_validation, y_validation, x_test, y_test
 
 
-def load_dataset_generator():
-    (x_train, y_train), (x_test, y_test) = cifar10.load_data()
+def load_dataset_generator(test_set):
+    (x_train, y_train), (x_test, y_test) = get_dataset(test_set)
+    num_classes = get_num_classes(test_set)
+
     x_train = normalise(x_train)
     x_test = normalise(x_test)
 
-    y_train = keras.utils.to_categorical(y_train, NUM_CLASSES)
-    y_test = keras.utils.to_categorical(y_test, NUM_CLASSES)
+    y_train = keras.utils.to_categorical(y_train, num_classes)
+    y_test = keras.utils.to_categorical(y_test, num_classes)
 
     datagen = ImageDataGenerator(
         featurewise_center=False,  # set input mean to 0 over the dataset
@@ -71,9 +119,10 @@ def load_dataset_generator():
     return x_train, y_train, x_test, y_test, train_generator, valid_generator
 
 
-def make_model():
+def make_model(test_set):
+    num_classes = get_num_classes(test_set)
+
     model = Sequential()
-    # to check
     model.add(Conv2D(16, (3, 3), padding='same', activation='relu', input_shape=(32, 32, 3)))
     model.add(Conv2D(32, (3, 3), padding='same', activation='relu', input_shape=(32, 32, 3)))
     model.add(MaxPooling2D(pool_size=(2, 2), strides=None, padding='valid', data_format=None))
@@ -84,13 +133,15 @@ def make_model():
     model.add(Dropout(0.25))
     model.add(Flatten())
     model.add(Dense(256, input_shape=(256,)))
-    model.add(Dense(NUM_CLASSES))
+    model.add(Dense(num_classes))
     model.add(LeakyReLU(0.1))
     model.add(Activation('softmax'))
     return model
 
 
-def make_model_3_block():
+def make_model_3_block(test_set):
+    num_classes = get_num_classes(test_set)
+
     model = Sequential()
     model.add(
         Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same', input_shape=(32, 32, 3)))
@@ -110,7 +161,9 @@ def make_model_3_block():
 
     model.add(Flatten())
     model.add(Dense(128, activation='relu', kernel_initializer='he_uniform'))
-    model.add(Dense(NUM_CLASSES, activation='softmax'))
+
+    model.add(Dense(num_classes, activation='softmax'))
+    return model
 
 
 def set_memory_usage():
@@ -122,19 +175,19 @@ def set_memory_usage():
 def main():
     set_memory_usage()
 
-    model = make_model()
+    model = make_model_3_block(SET)
     model.compile(loss='categorical_crossentropy',  # we train 10-way classification
                   optimizer=keras.optimizers.adamax(lr=INIT_LR),  # for SGD
                   metrics=['accuracy'])  # report accuracy during training
 
     if USE_GENERATOR:
-        x_train, y_train, x_test, y_test, train_gen, valid_gen = load_dataset_generator()
+        x_train, y_train, x_test, y_test, train_gen, valid_gen = load_dataset_generator(SET)
         history = model.fit_generator(generator=train_gen, validation_data=valid_gen,
                                       steps_per_epoch=len(train_gen), validation_steps=len(valid_gen),
-                                      epochs=EPOCHS, shuffle=True, verbose=1)
+                                      epochs=EPOCHS, shuffle=True, verbose=2)
 
     else:
-        x_train, y_train, x_validation, y_validation, x_test, y_test = load_dataset()
+        x_train, y_train, x_validation, y_validation, x_test, y_test = load_dataset(SET)
 
         history = model.fit(
             x_train, y_train,  # prepared data
@@ -148,14 +201,18 @@ def main():
     _, acc = model.evaluate(x_test, y_test, batch_size=BATCH_SIZE)
     print("Accuracy = %.2f " % (acc * 100.0) + "%")
     prediction = model.predict_classes(x_test, batch_size=BATCH_SIZE)
-    show_confusion_matrix(prediction, np.argmax(y_test, axis=1), cifar10_classes)
+
+    show_confusion_matrix(prediction, np.argmax(y_test, axis=1), get_labels(SET), SET)
     summarize_diagnostics(history)
 
 
-def show_confusion_matrix(y_pred, y_real, target_names):
+def show_confusion_matrix(y_pred, y_real, target_names,test_set):
     cm = confusion_matrix(y_pred, y_real)
     cmap = plt.get_cmap('Blues')
-    plt.figure(figsize=(8, 6))
+    if test_set is TestSet.CIFAR10:
+        plt.figure(figsize=(8, 6))
+    else:
+        plt.figure(figsize=(40, 30))
     plt.imshow(cm, interpolation='nearest', cmap=cmap)
     plt.title("Macierz pomyłek")
     plt.colorbar()
